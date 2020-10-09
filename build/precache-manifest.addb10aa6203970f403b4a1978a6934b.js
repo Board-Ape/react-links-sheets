474bee3ec23a2eaed668aaf10d6ef80f972cd6a1
Samuel Singer <samuel.singer1@gmail.com>
2020-10-09 09:34:31 -0600

    bug: fixing bugs

---
+++ build/precache-manifest.addb10aa6203970f403b4a1978a6934b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa51bec64aea6c75952d57b4cc376682",
+    "url": "/react-links-sheets/index.html"
+  },
+  {
+    "revision": "bb6786c0de6188ac196e",
+    "url": "/react-links-sheets/static/css/main.ef5425e5.chunk.css"
+  },
+  {
+    "revision": "357ab7c6ae0a842a576d",
+    "url": "/react-links-sheets/static/js/2.66d33258.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-links-sheets/static/js/2.66d33258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6786c0de6188ac196e",
+    "url": "/react-links-sheets/static/js/main.237a97cf.chunk.js"
+  },
+  {
+    "revision": "36badfcb8283acbfc39b",
+    "url": "/react-links-sheets/static/js/runtime-main.6e011324.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-links-sheets/static/media/logo.5d5d9eef.svg"
+  }
+]);
